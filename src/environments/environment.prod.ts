3a388a4e023e3e0dc963b94d50d50c67716615ab
sumanpaul2031@gmail.com <sumanpaul2031@gmail.com>
2020-05-03 19:41:27 +0530

    ngBlog version 0.0.1 completed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJau4_drcVPo-f_DrTu09Sn3eLacxDPqM",
+    authDomain: "ngblog-6b034.firebaseapp.com",
+    databaseURL: "https://ngblog-6b034.firebaseio.com",
+    projectId: "ngblog-6b034",
+    storageBucket: "ngblog-6b034.appspot.com",
+    messagingSenderId: "359858193072",
+    appId: "1:359858193072:web:86d2d2f059a3a40c533175",
+    measurementId: "G-PQEYBP4ZCX"
+  }
 };
